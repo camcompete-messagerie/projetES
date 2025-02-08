6f081c9652e6bcf3daca81c18bacab049cdf639f
camcompete-messagerie <messageriecamcompete@gmail.com>
2025-02-08 09:16:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("corps1").addEventListener("click", function(){
+    window.location.href = "Première page.html"
+})
+
+document.getElementById("corps2").addEventListener("click", function(){
+    window.location.href = "Deuxieme page.html"
+})
+
+document.getElementById("corps3").addEventListener("click", function(){
+    window.location.href = "Troisieme page.html"
+})
